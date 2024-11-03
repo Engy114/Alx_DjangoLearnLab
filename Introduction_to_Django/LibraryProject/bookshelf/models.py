c83e22d93b0b7c655b9948b0befb9d8cba776555
Engy114 <engyrafat114@gmail.com>
2024-11-04 00:45:36 +0200

    add changes

--- Introduction_to_Django/LibraryProject/bookshelf/models.py
+++ Introduction_to_Django/LibraryProject/bookshelf/models.py
@@ -4,17 +4,3 @@ class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
     publication_year = models.IntegerField()
-
-# Create a new book instance
-book = Book.objects.create(title="1984", author="George Orwell", publication_year=1949)
-
-# Retrieve the book
-book = Book.objects.get(title="1984")
-print(book.title, book.author, book.publication_year)
-
-# Update the title of the book
-book.title = "Nineteen Eighty-Four"
-book.save()
-
-# Delete the book
-book.delete()
