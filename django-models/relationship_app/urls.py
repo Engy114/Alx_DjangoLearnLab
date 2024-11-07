93d8f1fb4cbcb6da7a52005817a8d0e8b3291733
Engy114 <engyrafat114@gmail.com>
2024-11-07 23:40:12 +0200

    save changes

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -20,3 +20,12 @@ urlpatterns = [
     # URL for logout, specifying the template explicitly if required
     path('logout/', LogoutView.as_view(template_name="relationship_app/logout.html"), name='logout'),
 ]
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('admin/', views.admin_view, name='admin_view'),
+    path('librarian/', views.librarian_view, name='librarian_view'),
+    path('member/', views.member_view, name='member_view'),
+]
