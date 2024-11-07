5bb4e0fa4db67eed1241579fb84d61801a95d2a5
Engy114 <engyrafat114@gmail.com>
2024-11-07 23:32:50 +0200

    save changes

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -14,21 +14,17 @@ def list_books(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
-from django.contrib.auth.views import LoginView, LogoutView
-from django.urls import reverse_lazy
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
-from django.views.generic import CreateView
 from django.contrib.auth import login
 
-class UserLoginView(LoginView):
-    template_name = 'relationship_app/login.html'
-
-class UserLogoutView(LogoutView):
-    next_page = reverse_lazy('login')  # Redirect to login after logout
-
-class UserRegisterView(CreateView):
-    template_name = 'relationship_app/register.html'
-    form_class = UserCreationForm
-    success_url = reverse_lazy('login')  # Redirect to login after successful registration
-
-
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # Log the user in after registration
+            return redirect('home')  # Redirect to a homepage or any other page you prefer
+    else:
+        form = UserCreationForm()
+    return render(request, 'relationship_app/register.html', {'form': form})
