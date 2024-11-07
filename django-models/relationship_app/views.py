b8534b6bfbba8906b83623f73356493a977bdc9f
Engy114 <engyrafat114@gmail.com>
2024-11-07 20:26:31 +0200

    save changes

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,21 +1,14 @@
-from django.shortcuts import render
+from django.views.generic import DetailView, ListView
+from .models import Library, Book
 
-from .models import Book
+# Class-based view for displaying library details
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'  # Using specified template
+    context_object_name = 'library'
 
+# Example function-based view for listing all books and their authors
 def list_books(request):
     books = Book.objects.all()
-    return render(request, 'list_books.html', {'books': books})
-
-from django.views.generic import ListView
-from .models import Book, Library
-
-class LibraryBooksView(ListView):
-    model = Book
-    template_name = 'library_books.html'
-
-    def get_queryset(self):
-        library_id = self.kwargs.get('library_id')
-        return Book.objects.filter(library__id=library_id)
-    
-    "relationship_app/list_books.html"
+    return render(request, 'relationship_app/list_books.html', {'books': books})
 
