da97a26f05dd4e8abb2a9aeeaba602500ab6d776
Engy114 <engyrafat114@gmail.com>
2024-11-13 17:47:32 +0200

    save changes

--- advanced_features_and_security/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bookshelf',
-    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -123,6 +122,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-AUTH_USER_MODEL = 'accounts.CustomUser'
-AUTH_USER_MODEL = 'bookshelf.CustomUser'
