d4e3ae56eee1ee2f3d50ca5080b51c83590534f3
Engy114 <engyrafat114@gmail.com>
2024-11-13 19:36:38 +0200

    save changes

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,13 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 
+# Book Model
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
     publication_year = models.IntegerField()
 
-    from django.contrib.auth.models import AbstractUser, BaseUserManager
-from django.db import models
+    def __str__(self):
+        return self.title
 
+# Custom User Manager
 class CustomUserManager(BaseUserManager):
     def create_user(self, username, email, password=None, **extra_fields):
         if not email:
@@ -29,6 +32,7 @@ class CustomUserManager(BaseUserManager):
 
         return self.create_user(username, email, password, **extra_fields)
 
+# Custom User Model
 class CustomUser(AbstractUser):
     date_of_birth = models.DateField(null=True, blank=True)
     profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
@@ -38,8 +42,7 @@ class CustomUser(AbstractUser):
     def __str__(self):
         return self.username
 
-        from django.db import models
-
+# Document Model with Custom Permissions
 class Document(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
@@ -51,5 +54,9 @@ class Document(models.Model):
             ("can_edit", "Can edit document"),
             ("can_delete", "Can delete document"),
         ]
+
+    def __str__(self):
+        return self.title
+
 
 
