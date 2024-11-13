39c1595e5304ea6eb4cfa01a07e01dfa798e510c
Engy114 <engyrafat114@gmail.com>
2024-11-13 17:30:08 +0200

    save changes

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -9,3 +9,17 @@ class BookAdmin(admin.ModelAdmin):
 admin.site.register(Book, BookAdmin)
 
 "admin.ModelAdmin"
+
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser  # Import the CustomUser model
+
+class CustomUserAdmin(UserAdmin):
+    # Extend the UserAdmin to display custom fields
+    model = CustomUser
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
+    )  # Adds date_of_birth and profile_photo to the admin form
+
+# Register the CustomUser model with the CustomUserAdmin
+admin.site.register(CustomUser, CustomUserAdmin)
