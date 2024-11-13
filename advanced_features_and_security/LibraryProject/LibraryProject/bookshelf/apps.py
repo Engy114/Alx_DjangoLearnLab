265cb7352a74de4e6bd84ceb40c232fd7a7ea1fd
Engy114 <engyrafat114@gmail.com>
2024-11-13 17:43:42 +0200

    save changes

--- advanced_features_and_security/LibraryProject/LibraryProject/bookshelf/apps.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/bookshelf/apps.py
@@ -4,3 +4,8 @@ from django.apps import AppConfig
 class BookshelfConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'bookshelf'
+
+from django.apps import AppConfig
+
+class BookshelfConfig(AppConfig):
+    name = 'bookshelf'  # Ensure this matches exactly
