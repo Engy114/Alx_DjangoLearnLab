bcccfcb52b33a64900f8a54c2d698fd44de87783
Engy114 <engyrafat114@gmail.com>
2024-11-13 18:48:49 +0200

    save changes

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -38,3 +38,18 @@ class CustomUser(AbstractUser):
     def __str__(self):
         return self.username
 
+        from django.db import models
+
+class Document(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+
+    class Meta:
+        permissions = [
+            ("can_view", "Can view document"),
+            ("can_create", "Can create document"),
+            ("can_edit", "Can edit document"),
+            ("can_delete", "Can delete document"),
+        ]
+
+
