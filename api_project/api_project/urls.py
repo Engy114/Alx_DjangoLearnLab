bb95ba689d75389536ebc8d270d14f89d0e443a6
Engy114 <engyrafat114@gmail.com>
2024-11-24 15:23:07 +0200

    save changes

--- api_project/api_project/urls.py
+++ api_project/api_project/urls.py
@@ -30,4 +30,10 @@ from django.urls import path, include
 
 urlpatterns = [
     path('api/', include('api.urls')),  # Includes URLs from the `api` app
+    from django.urls import path, include
+
+urlpatterns = [
+    path('api/', include('api.urls')),
+]
+
 ]
