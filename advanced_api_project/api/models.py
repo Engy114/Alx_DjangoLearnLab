a4cb7ac449fe8048d314f3384db8a0a081a61620
Engy114 <engyrafat114@gmail.com>
2024-12-01 15:15:00 +0200

    save changes

---
+++ advanced_api_project/api/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+from django.db import models
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    publication_year = models.IntegerField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
+
+    def __str__(self):
+        return self.title
+
+class Author(models.Model):
+    """
+    Represents an author with a name.
+    """
+    name = models.CharField(max_length=100)
+
+class Book(models.Model):
+    """
+    Represents a book with a title, publication year, and associated author.
+    """
+    title = models.CharField(max_length=100)
+    publication_year = models.IntegerField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
