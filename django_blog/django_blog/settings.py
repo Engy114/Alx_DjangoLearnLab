47b6d49c88a26f55fe904c052ca26e93d6311246
Engy114 <engyrafat114@gmail.com>
2024-12-04 01:47:24 +0200

    save changes

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -122,6 +122,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",  # This points to the 'static' directory in your project
+]
+STATIC_ROOT = BASE_DIR / "staticfiles"  # Optional for production
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
