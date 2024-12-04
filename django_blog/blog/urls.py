edf26a2ea2073b928f762efe6e2a90420ac68f17
Engy114 <engyrafat114@gmail.com>
2024-12-04 04:37:36 +0200

    Save

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -31,7 +31,7 @@ urlpatterns = [
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),  # Delete a post
 
     # CRUD URLs for Comment model
-    path('post/<int:post_id>/comments/new/', CommentCreateView.as_view(), name='comment-create'),  # Create a new comment
+    path('posts/<int:post_id>/comments/new/', CommentCreateView.as_view(), name='comment-create'),  # Create a new comment
     path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='comment-update'),  # Update a comment
     path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),  # Delete a comment
 
