c3634645b987df38ecedcbd66a2aab0cd938042c
Engy114 <engyrafat114@gmail.com>
2024-12-04 02:49:58 +0200

    save

--- django_blog/django_blog/urls.py
+++ django_blog/django_blog/urls.py
@@ -15,6 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
-
+urlpatterns = [
+    path('admin/', admin.site.urls),          # Admin site URL
+    path('', include('blog.urls')),          # Include blog app URLs for the main site
+]
