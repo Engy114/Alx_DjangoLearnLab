74ac71f112e3b77fdae4b6e30e6ada6bf3ecc8d1
Engy114 <engyrafat114@gmail.com>
2024-12-10 22:06:34 +0200

    Save

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from rest_framework.authtoken.models import Token
+from rest_framework import serializersCharField
 
 User = get_user_model()
 
