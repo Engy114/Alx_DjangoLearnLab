afde1b34b4f0fdda8316e995b344d50a70c553f5
Engy114 <engyrafat114@gmail.com>
2024-12-11 02:19:48 +0200

    Save

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -5,7 +5,6 @@ from rest_framework.authtoken.models import Token
 User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
-    # Define password explicitly as it needs special handling
     password = serializers.CharField(write_only=True, required=True, style={'input_type': 'password'})
     confirm_password = serializers.CharField(write_only=True, required=True, style={'input_type': 'password'})
 
@@ -14,15 +13,11 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ['id', 'username', 'email', 'password', 'confirm_password', 'bio', 'profile_picture', 'followers']
 
     def create(self, validated_data):
-        # Ensure passwords match
         password = validated_data.pop('password')
         confirm_password = validated_data.pop('confirm_password')
         if password != confirm_password:
             raise serializers.ValidationError({"password": "Passwords must match."})
 
-        # Create the user
-        user = User.objects.create_user(**validated_data)
-        
-        # Create an authentication token for the user
-        Token.objects.create(user=user)
+        user = User.objects.create_user(**validated_data, password=password)
+        Token.objects.create(user=user)  # Generate token for the user
         return user
