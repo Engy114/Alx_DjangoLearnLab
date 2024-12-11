93483ddd09ce731238aecb19ddefa0018ea9d6e0
Engy114 <engyrafat114@gmail.com>
2024-12-11 04:16:28 +0200

    save

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -2,10 +2,19 @@ from django.db import models
 from django.conf import settings
 
 class Post(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="posts")
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='posts')
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.user.username} - {self.created_at}"
+        return f"{self.user.username} - {self.content[:20]}"
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='comments')
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Comment by {self.user.username} on {self.post.id}"
 
