08338f0248c8523df16aa464ef2e3d52498cf082
Engy114 <engyrafat114@gmail.com>
2024-12-11 04:21:43 +0200

    save

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,19 +1,43 @@
-from django.shortcuts import render
-from rest_framework.views import APIView
-from accounts.views import status
-from rest_framework.response import Response
+from rest_framework import viewsets, permissions
 from rest_framework.permissions import IsAuthenticated
-from .models import Post
+from .models import Post, Comment
+from accounts.serializers import PostSerializer, CommentSerializer
 
-class FeedView(APIView):
-    permission_classes = [IsAuthenticated]
 
-    def get(self, request):
-        # Get posts from users the current user is following
-        following_users = request.user.following.all()
-        posts = Post.objects.filter(user__in=following_users).order_by('-created_at')
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    """
+    Custom permission to only allow owners of an object to edit or delete it.
+    """
+    def has_object_permission(self, request, view, obj):
+        # Read permissions are allowed for any request
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        # Write permissions are only allowed to the owner of the object
+        return obj.user == request.user
 
-        # Serialize and return posts
-        feed = [{"user": post.user.username, "content": post.content, "created_at": post.created_at} for post in posts]
-        return Response(feed, status=status.HTTP_200_OK)
 
+class PostViewSet(viewsets.ModelViewSet):
+    """
+    ViewSet for CRUD operations on posts.
+    """
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
+
+    def perform_create(self, serializer):
+        # Set the user as the creator of the post
+        serializer.save(user=self.request.user)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    """
+    ViewSet for CRUD operations on comments.
+    """
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
+
+    def perform_create(self, serializer):
+        # Set the user as the creator of the comment
+        post_id = self.request.data.get('post')
+        serializer.save(user=self.request.user, post_id=post_id)
