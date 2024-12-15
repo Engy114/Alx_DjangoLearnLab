b1f11e3c27f13f32ade2817a7d837f961d2a26c2
Engy114 <engyrafat114@gmail.com>
2024-12-15 20:04:30 +0200

    save

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,10 +1,47 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, viewsets, permissions
 from rest_framework.permissions import IsAuthenticated
-from .models import Post
-from accounts.serializers import PostSerializer
-from accounts.views import permissions,IsAuthenticated
+from .models import Post, Comment
+from accounts.serializers import PostSerializer, CommentSerializer
+
+
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    """
+    Custom permission to only allow owners of an object to edit or delete it.
+    """
+    def has_object_permission(self, request, view, obj):
+        # Read permissions are allowed for any request
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        # Write permissions are only allowed to the owner of the object
+        return obj.author == request.user
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    """
+    ViewSet for CRUD operations on posts.
+    """
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
+
+    def perform_create(self, serializer):
+        # Automatically set the current user as the post author
+        serializer.save(author=self.request.user)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    """
+    ViewSet for CRUD operations on comments.
+    """
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
+
+    def perform_create(self, serializer):
+        # Automatically set the current user as the comment author
+        serializer.save(author=self.request.user)
 
 
 class FeedView(APIView):
@@ -12,7 +49,7 @@ class FeedView(APIView):
     Returns posts from users that the authenticated user is following.
     Posts are ordered by creation date, showing the most recent first.
     """
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [IsAuthenticated]
 
     def get(self, request):
         # Retrieve users the current user is following
