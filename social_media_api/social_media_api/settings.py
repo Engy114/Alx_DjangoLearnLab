a449179dd4c5f4b08185365663a29ced3d931f57
Engy114 <engyrafat114@gmail.com>
2024-12-15 20:18:24 +0200

    save

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -81,6 +81,13 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'your_db_name',
+        'USER': 'your_db_user',
+        'PASSWORD': 'your_db_password',
+        'HOST': 'your_db_host',
+        'PORT': '5432',
+    
     }
 }
 
@@ -127,3 +134,13 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'accounts.CustomUser'
+
+DEBUG = False
+ALLOWED_HOSTS = ['your-production-domain.com', 'your-server-ip']
+
+# Security settings
+SECURE_BROWSER_XSS_FILTER = True
+X_FRAME_OPTIONS = 'DENY'
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SECURE_SSL_REDIRECT = True  # Enable if HTTPS is configured
+
